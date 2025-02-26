0b6487c89cecfd437ef4c7fe8e399a9111585e48
Niels <nielsfreelancer7750@gmail.com>
2025-02-26 17:35:08 +0100

    Add descriptions

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,22 +4,19 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 local pid = vim.fn.getpid()
+local map = vim.keymap.set;
 
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local opts = { noremap = true, silent = false }
 
   -- Keybindings for useful LSP functions
-  buf_set_keymap('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)        -- Go to definition
-  buf_set_keymap('n', '<Leader>gk', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)             -- Show documentation
-  buf_set_keymap('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)   -- Go to implementation
-  buf_set_keymap('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)   -- Rename
-  buf_set_keymap('n', '<Leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)       -- Find references
-  buf_set_keymap('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts) -- Code actions
-  buf_set_keymap('n', '<Leader>[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts) -- Previous diagnostic
-  buf_set_keymap('n', '<Leader>]d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts) -- Next diagnostic
+  map('n', '<Leader>gd', '<cmd>lua vim.lsp.buf.definition()<CR>', {noremap=true, silent=false, desc="Go to definition"})
+  map('n', '<Leader>gk', '<cmd>lua vim.lsp.buf.hover()<CR>', {noremap=true, silent=false, desc="Show documentation"})
+  map('n', '<Leader>gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', {noremap=true, silent=false, desc="Go to implementation"})
+  map('n', '<Leader>rr', '<cmd>lua vim.lsp.buf.rename()<CR>', {noremap=true, silent=false, desc="Rename"})
+  map('n', '<Leader>gr', '<cmd>lua vim.lsp.buf.references()<CR>', {noremap=true, silent=false, desc="Find references"})
+  map('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', {noremap=true, silent=false, desc="Code actions"})
+  map('n', '<Leader>[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', {noremap=true, silent=false, desc="Previou diagnostic"})
+  map('n', '<Leader>]d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', {noremap=true, silent=false, desc="Next diagnostic"})
 end
 
 lspconfig.omnisharp.setup({
